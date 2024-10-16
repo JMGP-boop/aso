8243854f97fcf0a66b143423bc71ae440d2258c1
JMGP-boop <juagarpra2@alu.edu.gva.es>
2024-10-16 10:52:02 +0200

    Add files via upload

---
+++ JMGPscript4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Pedir al usuario el primer numero
+echo "Por favor, ingresa el primer numero:"
+read numero1
+
+#Pedir al usuario el segundo numero
+echo "Por favor, ingresa el segundo numero:"
+read numero2
+
+#Mostrar los numeros ingresados
+echo "Has ingresado los siguientes numeros:"
+echo "Primer numero: $numero1"
+echo "Segundo numero: $numero2"
+
+
